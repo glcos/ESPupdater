6f1413fe0abb834d992045c427fb9d5c1a996129
glcos <gianluigi@cosari.it>
2016-10-19 21:44:39 +0200

    Add files via upload

--- md5.php
+++ md5.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ESP application updater
- * Ver. 1.0
+ * Ver. 1.1
  *
  * Gianluigi Cosari - gianluigi@cosari.it
  * October 2016
@@ -14,11 +14,32 @@ define("AF_DIR", "appfiles/");
 $data = array();
 $hash_str = "";
 
-$files = scandir(AF_DIR, SCANDIR_SORT_ASCENDING);
+$appName = filter_input(INPUT_GET, "a", FILTER_SANITIZE_STRING);
+
+if (isset($appName)) {
+	// Sanitize input, remove unwanted characters
+	$unallowed = array("/", "\\", ".");
+	$appName = str_replace($unallowed, "", $appName);
+	$fullpath = AF_DIR . $appName . "/";
+} else {
+	$fullpath = AF_DIR;
+}
+
+if (!is_dir($fullpath)) {
+	echo "directory not found";
+	exit;
+}
+
+$files = scandir($fullpath, SCANDIR_SORT_ASCENDING);
+
+if (count($files) < 3) {
+	echo "no files found";
+	exit;	
+}
 
 foreach($files as $entry) {
 	if ($entry != "." && $entry != "..") {
-		$md5 = md5_file(AF_DIR . $entry);
+		$md5 = md5_file($fullpath . $entry);
 		$data[$entry] = $md5;
 		$hash_str .= $entry . $md5;
 	}
@@ -27,7 +48,7 @@ foreach($files as $entry) {
 $jlist = json_encode($data);
 $group_hash = md5($hash_str);
 
-file_put_contents("list.json", $jlist);
+file_put_contents($appName . ".json", $jlist);
 
 echo $group_hash;
 ?> 
